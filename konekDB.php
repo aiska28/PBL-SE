ad7cfb1b27eaa887dbb63c602ac740a75b14b2f9
aiska28 <aiskacaca8@gmail.com>
2025-12-09 14:06:18 +0700

    tadi error

--- konekDB.php
+++ konekDB.php
@@ -1,9 +1,9 @@
 <?php
 $host = "localhost";
-$port = "5434";
-$dbname = "softwareen";
+$port = "5432";
+$dbname = "softwareEn";
 $user = "postgres";
-$password = "ailsasahda13"; // ubah sesuai password PostgreSQL kamu
+$password = "aiska284"; // ubah sesuai password PostgreSQL kamu
 
 $conn = pg_connect("host=$host port=$port dbname=$dbname user=$user password=$password");
 if (!$conn) {
